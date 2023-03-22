b4618f8c9f98d43d42cd082bd8279f1ea41c2336
fokxon <fokxon@MacBook-Air-3.local>
2023-03-23 00:55:59 +0800

    stop using flake8

--- Lab04/app.py
+++ Lab04/app.py
@@ -13,25 +13,19 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--window-size=1920,1080')
 options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(service=ChromeService(
-    ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 # driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
 driver.get("https://www.nycu.edu.tw/")
 driver.maximize_window()
-WebDriverWait(driver, 60).until(
-    EC.element_to_be_clickable((By.LINK_TEXT, '新聞'))).click()
-WebDriverWait(driver, 60).until(EC.element_to_be_clickable(
-    (By.CSS_SELECTOR, 'li[class="su-post"] a'))).click()
-print(WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.CLASS_NAME, 'single-post-title'))).text)
+WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.LINK_TEXT, '新聞'))).click()
+WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li[class="su-post"] a'))).click()
+print(WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'single-post-title'))).text)
 for i in WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'entry-content'))):
     print(i.text)
 
 driver.switch_to.new_window('tab')
 driver.get("https://www.google.com")
-WebDriverWait(driver, 60).until(EC.element_to_be_clickable(
-    (By.CLASS_NAME, 'gLFyf'))).send_keys('310553035', Keys.RETURN)
-print(WebDriverWait(driver, 60).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, 'DKV0Md')))[1].text)
+WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CLASS_NAME, 'gLFyf'))).send_keys('310553035', Keys.RETURN)
+print(WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'DKV0Md')))[1].text)
 driver.quit()
