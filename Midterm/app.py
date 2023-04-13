9fc59cc9f47d5a0fa997b966b14cbccb28781cb9
fokxon <fokxon@MacBook-Air-3.local>
2023-04-13 16:08:47 +0800

    complete app.py

--- Midterm/app.py
+++ Midterm/app.py
@@ -1,37 +1,31 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+#from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.edge.service import Service as EdgeService
+#from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--window-size=1920,1080')
 options.add_argument('--disable-gpu')
-#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+#driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
 driver.get("https://docs.python.org/3/tutorial/index.html")
 driver.maximize_window()
-print(WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div [class="body"]')))[0].text)
-print(WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div [class="body"] p')))[0].text)
-#WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'option[value="zh-tw"]'))).click()
 
-"""
-print(WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'single-post-title'))).text)
-for i in WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'entry-content'))):
-    print(i.text)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/ul/li[7]/div[1]/select/option[9]'))).click()
 
-driver.switch_to.new_window('tab')
-driver.get("https://www.google.com")
-WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CLASS_NAME, 'gLFyf'))).send_keys('310553035', Keys.RETURN)
-print(WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'DKV0Md')))[1].text)
+print(driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div/section/h1').text)
+print(driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div/section/p[1]').text)
+
+WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/ul/li[11]/div/form/input[1]'))).send_keys('class', Keys.RETURN)
+WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.XPATH, '/html/body/div[3]/div[1]/div/div/div[1]/ul/li')))
+for i in range(5):
+    print(driver.find_elements(By.XPATH, '/html/body/div[3]/div[1]/div/div/div[1]/ul/li')[i].text)
 driver.quit()
-"""
