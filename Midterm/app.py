c10aebddc32b94a20d931d06cbdc71bb9b7fa7f9
fokxon <fokxon@MacBook-Air-3.local>
2023-04-13 16:45:46 +0800

    app print only search title

--- Midterm/app.py
+++ Midterm/app.py
@@ -21,11 +21,12 @@ driver.maximize_window()
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/ul/li[7]/div[1]/select/option[9]'))).click()
 
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[1]/div/div'))).click()
 print(driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div/section/h1').text)
 print(driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div/section/p[1]').text)
 
-WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/ul/li[11]/div/form/input[1]'))).send_keys('class', Keys.RETURN)
-WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.XPATH, '/html/body/div[3]/div[1]/div/div/div[1]/ul/li')))
-for i in range(5):
-    print(driver.find_elements(By.XPATH, '/html/body/div[3]/div[1]/div/div/div[1]/ul/li')[i].text)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/ul/li[11]/div/form/input[1]'))).send_keys('class', Keys.RETURN)
+WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '/html/body/div[3]/div[1]/div/div/div[1]/ul/li')))
+for i in range(1, 6):
+    print(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[1]/div/div/div[1]/ul/li[' + str(i) + ']/a'))).text)
 driver.quit()
